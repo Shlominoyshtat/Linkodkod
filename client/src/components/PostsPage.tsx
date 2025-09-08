11577cd6c1e1a562eba08d7563f3d6a03ddfac6d
shlomi noyshtat <s0583244814@gmail.com>
2025-09-08 17:48:48 +0300

    added design

--- client/src/components/PostsPage.tsx
+++ client/src/components/PostsPage.tsx
@@ -5,7 +5,7 @@ export default function PostsPage() {
   return (postArray.map((post) => (
     <section className='app'>
     <div id='post' key={post.id} className='main'>
-      <img src={post.img} alt="" />
+      <img src={post.img} alt="" id='phase'/>
       <figcaption>{post.figcaotion}</figcaption>
       <p>name: {post.name}</p>
       <p>Uploaded at: {post.date}</p>
