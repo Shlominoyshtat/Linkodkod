3616edf6ee79015c1262e2f12fef8910cfbdae3a
shlomi noyshtat <s0583244814@gmail.com>
2025-09-08 11:19:14 +0300

    Arranging folders and adding small features

---
+++ src/components/application-layout/header.tsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return(
+        <>
+        <header>
+        <h1>The soldiers' house ✨</h1>
+            <img id="iconKodkod" src="https://lh3.googleusercontent.com/drive-storage/AJQWtBOXUVB9l5FxI5zqdrYXbKkRHlSnwyk5yZnuwdSgB44ryCBMtP6VKRH9xeX_TkbZtaPxxAKGS_s-1pR11qpKcer82_rX_RnwFAOwQkXG_xzxJLg=w1920-h889" alt="" />
+        </header>
+        </>
+    )
+}
---
+++ src/components/application-layout/main.tsx
@@ -0,0 +1,13 @@
+import PostsPage from "../PostsPage";
+import Header from "./header";
+
+export default function Main() {
+  return (
+    <>
+      <Header />
+      <main>
+        <PostsPage />
+      </main>
+    </>
+  );
+}
