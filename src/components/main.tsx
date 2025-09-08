147e41f927c5428aecd02ee5acf8c9993ae93ae6
shlomi noyshtat <s0583244814@gmail.com>
2025-09-08 10:52:35 +0300

    Separation between main and header

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,3 +1,6 @@
+import Post from "./post";
+import PostsPage from "./PostsPage";
+
 export default function Main(){
     return(
         <>
@@ -5,6 +8,10 @@ export default function Main(){
         <h1>The soldiers' house ✨</h1>
             <img src="https://lh3.googleusercontent.com/drive-storage/AJQWtBOXUVB9l5FxI5zqdrYXbKkRHlSnwyk5yZnuwdSgB44ryCBMtP6VKRH9xeX_TkbZtaPxxAKGS_s-1pR11qpKcer82_rX_RnwFAOwQkXG_xzxJLg=w1920-h889" alt="" />
         </header>
+        <main>
+            <Post name='cheron cheri'/>
+            <PostsPage/>
+        </main>
         </>
     )
 }
